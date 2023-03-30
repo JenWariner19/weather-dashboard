f8a7a8394354dfda6efae1400143647ce8ed8495
Jen Wariner <jenniferwariner@yahoo.com>
2023-03-30 12:15:44 -0500

    added code for forecast and localstorage

--- assets/script.js
+++ assets/script.js
@@ -6,15 +6,36 @@ var currentTemp = document.getElementById("temperature");
 var currentHum = document.getElementById("humidity");
 var currentWS = document.getElementById("wind-speed");
 var currentIcon = document.getElementById("current-icon");
+var weatherHistory = JSON.parse(localStorage.getItem("cities"))||[];
+var currentDay = dayjs();
+console.log(weatherHistory);
 
+function displayHistory() {
+    var weatherHistory = JSON.parse(localStorage.getItem("cities"))||[];
+    document.querySelector("#saved").innerHTML = "";
+    for(var i=0; i < weatherHistory.length; i++) {
+        var button = document.createElement("button")
+        button.className = "col m-2 forecast badge bg-primary text-white rounded";
+        button.innerText = weatherHistory[i].city + "," + weatherHistory[i].state;
+        button.addEventListener("click", function(event) {
+            var cityState = event.target.innerText.split(",");  
+            getWeather(cityState[0], cityState[1])
+        })
+        document.querySelector("#saved").appendChild(button)
+    }
+}
+displayHistory()
 
-function currentWeather() {
+function getWeather(prevCity, prevState) {
     var cityValue = cityInput.value;
     var cityState = cityValue.split(",");
-    var city = cityState[0];
-    var state = cityState[1];
+    var city = prevCity|| cityState[0];
+    var state = prevState ||cityState[1];
     var country = "US";
     var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + "," + country + "&appid=" + apiKey;
+    weatherHistory.push({city,state})
+    localStorage.setItem("cities", JSON.stringify(weatherHistory))
+    displayHistory()
 
     fetch(requestUrl)
     .then(function (response) {
@@ -24,24 +45,51 @@ function currentWeather() {
         var lat = data[0].lat
         var lon = data[0].lon
         var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+        var futureWeather = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+
         fetch(currentWeatherUrl)
         .then(function(response){
             return response.json();
         })
         .then(function(data){
-            var cityName = data.name;
-            var temp = data.main.temp;
-            var wind = data.wind.speed;
-            var humidity = data.main.humidity;
-            var weatherIcon = data.weather[0].icon;
+            cityRequest.textContent = data.name + " - " + currentDay.format(" MMM DD, YYYY");
+            currentTemp.textContent = "Temp: " + data.main.temp + " F";
+            currentWS.textContent = "Wind: " + data.wind.speed;
+            currentHum.textContent = "Humidity: " + data.main.humidity;
+            currentIcon.src = "https://openweathermap.org/img/wn/"+data.weather[0].icon+".png";
 
-            cityRequest.textContent = cityName;
-            currentIcon.textContent = weatherIcon;
-            currentTemp.textContent = "Temp: " + temp;
-            currentWS.textContent = "Wind: " + wind;
-            currentHum.textContent = "Humidity: " + humidity;
         })
+        fetch(futureWeather)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            for(var i = 7; i < data.list.length; i+=8) {
+                console.log(data.list[i]);
+                var div = document.createElement("div")
+                div.className = "col m-2 forecast badge bg-primary text-white rounded";
+                var date = document.createElement("p");
+                date.innerText = dayjs.unix(data.list[i].dt).format(" MMM DD");
+                div.appendChild(date);
+                var image = document.createElement("img");
+                image.src = "https://openweathermap.org/img/wn/"+data.list[i].weather[0].icon+".png";
+                div.appendChild(image);
+                var temp = document.createElement("p");
+                temp.innerText = "Temp: " + data.list[i].main.temp + " F";
+                div.appendChild(temp);
+                var wind = document.createElement("p");
+                wind.innerText = "Wind: " + data.list[i].wind.speed;
+                div.appendChild(wind);
+                var hum = document.createElement("p");
+                hum.innerText = "Humidity: " + data.list[i].main.humidity;
+                div.appendChild(hum);
+
+                document.querySelector("#future-container").appendChild(div);
+            }
+    })
     })
 }
 
-searchBtn.addEventListener("click", currentWeather);
+
+searchBtn.addEventListener("click", getWeather);
